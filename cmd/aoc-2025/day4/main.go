8c46bdfe31bdd0f4bc496d75603d6d0286b9a2e5
Channing Ko-Madden <channing@regrow.ag>
2026-04-01 18:06:45 -0400

    part 2 done

--- cmd/aoc-2025/day4/main.go
+++ cmd/aoc-2025/day4/main.go
@@ -29,16 +29,6 @@ A A
 
 */
 
-/*
-Idea:
-Parse input into a 2D slice
-
-Create a struct to encapsulate accessing adjacent positions?
-
-Count number of positions with less than 4 adjacent rolls.
-
-*/
-
 func main() {
 
 	inputFile := flag.String("input", "", "Input file")
@@ -49,36 +39,13 @@ func main() {
 		return
 	}
 
-	occupancy_map := parseLines(internal.ReadAndParseData(*inputFile))
+	occupancyMap := parseLines(internal.ReadAndParseData(*inputFile))
 
-	maxRow := len(occupancy_map) - 1
-	maxCol := len(occupancy_map[0]) - 1
+	part1Result := part1(occupancyMap)
+	part2Result := part2(occupancyMap)
 
-	accessableRolls := 0
-	for rowNum, col := range occupancy_map {
-		for colNum := range col {
-			if occupancy_map[rowNum][colNum] == 1 {
-				point := Part1Point{
-					row: rowNum,
-					col: colNum,
-				}
-
-				neighbors := point.get_neighbors(maxRow, maxCol)
-
-				neighbor_count := 0
-				for _, n := range neighbors {
-					neighbor_count += occupancy_map[n.row][n.col]
-				}
-
-				if neighbor_count < 4 {
-					accessableRolls += 1
-				}
-			}
-
-		}
-	}
-
-	println(accessableRolls)
+	println("Part 1 result: ", part1Result)
+	println("Part 2 result: ", part2Result)
 }
 
 func parseLine(line string) []int {
@@ -114,52 +81,124 @@ func printMap(occupancyMap [][]int) {
 
 }
 
-type Part1Point struct {
+// The forklifts can only access a roll of paper if there are fewer
+// than four rolls of paper in the eight adjacent positions
+func part1(occupancyMap [][]int) int {
+	maxRow := len(occupancyMap) - 1
+	maxCol := len(occupancyMap[0]) - 1
+
+	accessableRolls := 0
+	for rowNum, col := range occupancyMap {
+		for colNum := range col {
+			if occupancyMap[rowNum][colNum] == 1 {
+				point := Point{
+					row: rowNum,
+					col: colNum,
+				}
+
+				neighbors := point.get_neighbors(maxRow, maxCol)
+
+				neighbor_count := 0
+				for _, n := range neighbors {
+					neighbor_count += occupancyMap[n.row][n.col]
+				}
+
+				if neighbor_count < 4 {
+					accessableRolls += 1
+				}
+			}
+
+		}
+	}
+
+	return accessableRolls
+}
+
+func part2(occupancyMap [][]int) int {
+	maxRow := len(occupancyMap) - 1
+	maxCol := len(occupancyMap[0]) - 1
+
+	removableRolls := 0
+	rollWasRemoved := true
+	//rollsToRemove := make([]Point, 0)
+	for rollWasRemoved {
+		rollWasRemoved = false
+		for rowNum, col := range occupancyMap {
+			for colNum := range col {
+				if occupancyMap[rowNum][colNum] == 1 {
+					point := Point{
+						row: rowNum,
+						col: colNum,
+					}
+
+					neighbors := point.get_neighbors(maxRow, maxCol)
+
+					neighbor_count := 0
+					for _, n := range neighbors {
+						neighbor_count += occupancyMap[n.row][n.col]
+					}
+
+					if neighbor_count < 4 {
+						removableRolls += 1
+						rollWasRemoved = true
+						// remove the roll from the map
+						occupancyMap[point.row][point.col] = 0
+					}
+				}
+
+			}
+		}
+	}
+
+	return removableRolls
+}
+
+type Point struct {
 	row int
 	col int
 }
 
-func (p *Part1Point) get_neighbors(maxRow int, maxCol int) []Part1Point {
+func (p *Point) get_neighbors(maxRow int, maxCol int) []Point {
 	row_above := p.row > 0
 	row_below := p.row < maxRow
 
 	col_left := p.col > 0
 	col_right := p.col < maxCol
 
-	neighbors := make([]Part1Point, 0, 6)
+	neighbors := make([]Point, 0, 6)
 
 	if row_above {
-		neighbors = append(neighbors, Part1Point{row: p.row - 1, col: p.col})
+		neighbors = append(neighbors, Point{row: p.row - 1, col: p.col})
 
 		if col_left {
-			neighbors = append(neighbors, Part1Point{row: p.row - 1, col: p.col - 1})
+			neighbors = append(neighbors, Point{row: p.row - 1, col: p.col - 1})
 		}
 
 		if col_right {
-			neighbors = append(neighbors, Part1Point{row: p.row - 1, col: p.col + 1})
+			neighbors = append(neighbors, Point{row: p.row - 1, col: p.col + 1})
 		}
 	}
 
 	if row_below {
-		neighbors = append(neighbors, Part1Point{row: p.row + 1, col: p.col})
+		neighbors = append(neighbors, Point{row: p.row + 1, col: p.col})
 
 		if col_left {
-			neighbors = append(neighbors, Part1Point{row: p.row + 1, col: p.col - 1})
+			neighbors = append(neighbors, Point{row: p.row + 1, col: p.col - 1})
 		}
 
 		if col_right {
-			neighbors = append(neighbors, Part1Point{row: p.row + 1, col: p.col + 1})
+			neighbors = append(neighbors, Point{row: p.row + 1, col: p.col + 1})
 		}
 
 	}
 
 	if col_left {
-		neighbors = append(neighbors, Part1Point{row: p.row, col: p.col - 1})
+		neighbors = append(neighbors, Point{row: p.row, col: p.col - 1})
 
 	}
 
 	if col_right {
-		neighbors = append(neighbors, Part1Point{row: p.row, col: p.col + 1})
+		neighbors = append(neighbors, Point{row: p.row, col: p.col + 1})
 	}
 
 	return neighbors
